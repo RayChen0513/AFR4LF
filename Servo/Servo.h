9760d328937516a979f3c71c9b36afa14b6e92bd
RayChen0513 <chenrayray20@gmail.com>
2025-12-27 21:13:10 +0800

    update path

--- Servo/Servo.h
+++ Servo/Servo.h
@@ -3,12 +3,12 @@
 // ===== ESP32 =====
 #if defined(ARDUINO_ARCH_ESP32)
 
-  #include "esp32/ESP32Servo.h"
+  #include "esp32/src/ESP32Servo.h"
   using Servo = ESP32Servo;
 
 // ===== 非 ESP32（AVR / R4 / SAMD / etc.）=====
 #else
 
-  #include "arduino/Servo.h"
+  #include "arduino/src/Servo.h"
 
 #endif
